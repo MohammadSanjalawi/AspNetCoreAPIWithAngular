cddf0c38041eceb6a791cff17b80147cb3865b35
mohammadsanjalawi <m7md-sanjalawi@hotmail.com>
2022-06-11 01:17:22 +0300

    Call Extension Methods

--- AccountOwnerServerAPI/Startup.cs
+++ AccountOwnerServerAPI/Startup.cs
@@ -1,5 +1,7 @@
+using AccountOwnerServerAPI.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -25,6 +27,8 @@ namespace AccountOwnerServerAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
 
             services.AddControllers();
         }
@@ -39,7 +43,16 @@ namespace AccountOwnerServerAPI
 
             app.UseHttpsRedirection();
 
+
+            app.UseStaticFiles();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                    ForwardedHeaders = ForwardedHeaders.All
+            });
+
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
